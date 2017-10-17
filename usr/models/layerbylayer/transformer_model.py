8b49652d884e122eb1e456bc3811307bdfbcf2b1
F. Stahlberg <fs439@jabba>
2017-10-17 03:27:16 +0100

    - Actually add transformer layer-by-layer implementation

---
+++ usr/models/layerbylayer/transformer_model.py
@@ -0,0 +1,346 @@
+# coding=utf-8
+"""Layer-by-layer variant of the transformer (attention) model. Some
+code is copied from tensor2tensor.models.transformer.
+
+encoder: [Self-Attention, Feed-forward] x n
+decoder: [Self-Attention, Source-Target-Attention, 
+          TargetRoot-Target-Attention, Feed-forward] x n
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Dependency imports
+
+from six.moves import xrange  # pylint: disable=redefined-builtin
+
+from tensor2tensor.layers import common_attention
+from tensor2tensor.layers import common_hparams
+from tensor2tensor.layers import common_layers
+from tensor2tensor.utils import expert_utils
+from tensor2tensor.utils import registry
+from tensor2tensor.utils import t2t_model
+
+import tensorflow as tf
+
+from usr import utils as usr_utils
+
+
+@registry.register_model
+class TransformerLayerbylayer(t2t_model.T2TModel):
+  """Transformer as layerbylayer model."""
+
+  def encode(self, inputs, target_space, hparams):
+    """Copied from tensor2tensor.models.transformer."""
+    inputs = common_layers.flatten4d3d(inputs)
+
+    encoder_input, self_attention_bias, encoder_decoder_attention_bias = (
+        transformer_prepare_encoder(inputs, target_space, hparams))
+
+    encoder_input = tf.nn.dropout(
+        encoder_input, 1.0 - hparams.layer_prepostprocess_dropout)
+
+    encoder_output = transformer_encoder(
+        encoder_input,
+        self_attention_bias,
+        hparams)
+
+    return encoder_output, encoder_decoder_attention_bias
+
+  def decode(
+      self,
+      decoder_input,
+      target_roots,
+      targets_is_pop,
+      encoder_output,
+      encoder_decoder_attention_bias,
+      decoder_self_attention_bias,
+      hparams,
+      cache=None):
+    """Decode Transformer outputs from encoder representation.
+
+    Args:
+      decoder_input: inputs to bottom of the model.
+          [batch_size, decoder_length, hidden_dim]
+      target_roots: a [batch_size, num_roots, embed_size] float32 tensor with 
+        target root embeddings
+      targets_is_pop: a [batch_size, targets_length] bool tensor with True if
+        the target label is POP and false otherwise
+      encoder_output: Encoder representation.
+          [batch_size, input_length, hidden_dim]
+      encoder_decoder_attention_bias: Bias and mask weights for
+          encoder-decoder attention. [batch_size, input_length]
+      decoder_self_attention_bias: Bias and mask weights for decoder
+          self-attention. [batch_size, decoder_length]
+      hparams: hyperparmeters for model.
+      cache: dict, containing tensors which are the results of previous
+          attentions, used for fast decoding.
+
+    Returns:
+      Final decoder representaiton. [batch_size, decoder_length, hidden_dim]
+    """
+    decoder_input = tf.nn.dropout(decoder_input,
+                                  1.0 - hparams.layer_prepostprocess_dropout)
+
+    decoder_output = transformer_decoder(
+        decoder_input,
+        target_roots,
+        targets_is_pop,
+        encoder_output,
+        decoder_self_attention_bias,
+        encoder_decoder_attention_bias,
+        hparams,
+        cache=cache)
+
+    # Expand since t2t expects 4d tensors.
+    return tf.expand_dims(decoder_output, axis=2)
+
+  def model_fn_body(self, features):
+    """Transformet main model_fn.
+
+    Args:
+      features: Map of features to the model. Should contain the following:
+          "inputs": Transformer inputs [batch_size, input_length, hidden_dim]
+          "tragets": Target decoder outputs.
+              [batch_size, decoder_length, hidden_dim]
+          "target_space_id"
+
+    Returns:
+      Final decoder representaiton. [batch_size, decoder_length, hidden_dim]
+    """
+    hparams = self._hparams
+
+    inputs = features["inputs"]
+
+    target_space = features["target_space_id"]
+    encoder_output, encoder_decoder_attention_bias = self.encode(
+        inputs, target_space, hparams)
+
+    targets = features["targets"]
+    targets = common_layers.flatten4d3d(targets)
+
+    decoder_input, decoder_self_attention_bias = transformer_prepare_decoder(
+        targets, hparams)
+    target_roots = features["target_roots"]
+    target_roots = common_layers.flatten4d3d(target_roots)
+    # Manage POP signals
+    if hparams.target_root_attention == "pop":
+        raw_targets = tf.squeeze(tf.squeeze(
+            features["raw_targets"], axis=2), axis=2)
+        targets_is_pop = tf.equal(raw_targets, hparams.pop_id)
+    else:
+        targets_is_pop = None
+
+    return self.decode(
+        decoder_input,
+        target_roots,
+        targets_is_pop,
+        encoder_output,
+        encoder_decoder_attention_bias,
+        decoder_self_attention_bias,
+        hparams)
+
+
+def transformer_decoder(decoder_input,
+                        target_roots,
+                        targets_is_pop,
+                        encoder_output,
+                        decoder_self_attention_bias,
+                        encoder_decoder_attention_bias,
+                        hparams,
+                        cache=None,
+                        name="decoder"):
+  """A stack of transformer layers.
+
+  Args:
+    decoder_input: a Tensor
+    target_roots: a [batch_size, num_roots, embed_size] float32 tensor with 
+      target root embeddings
+    targets_is_pop: a [batch_size, targets_length] bool tensor with True if
+      the target label is POP and false otherwise
+    encoder_output: a Tensor
+    decoder_self_attention_bias: bias Tensor for self-attention
+      (see common_attention.attention_bias())
+    encoder_decoder_attention_bias: bias Tensor for encoder-decoder attention
+      (see common_attention.attention_bias())
+    hparams: hyperparameters for model
+    cache: dict, containing tensors which are the results of previous
+        attentions, used for fast decoding.
+    name: a string
+
+  Returns:
+    y: a Tensors
+  """
+  x = decoder_input
+  extended_target_roots = usr_utils.expand_memory_by_pop(targets_is_pop, 
+                                                         target_roots,
+                                                         offset=1)
+  # Remove artefact from offset=1
+  extended_target_roots = extended_target_roots[:,:-1,:]
+  # Use target roots as additional input
+  x += extended_target_roots
+  with tf.variable_scope(name):
+    for layer in xrange(hparams.num_decoder_layers or
+                        hparams.num_hidden_layers):
+      layer_name = "layer_%d" % layer
+      layer_cache = cache[layer_name] if cache is not None else None
+      with tf.variable_scope(layer_name):
+        with tf.variable_scope("self_attention"):
+          y = common_attention.multihead_attention(
+              common_layers.layer_preprocess(x, hparams),
+              None,
+              decoder_self_attention_bias,
+              hparams.attention_key_channels or hparams.hidden_size,
+              hparams.attention_value_channels or hparams.hidden_size,
+              hparams.hidden_size,
+              hparams.num_heads,
+              hparams.attention_dropout,
+              attention_type=hparams.self_attention_type,
+              max_relative_position=hparams.max_relative_position,
+              cache=layer_cache)
+          x = common_layers.layer_postprocess(x, y, hparams)
+        if encoder_output is not None:
+          with tf.variable_scope("encdec_attention"):
+            # TODO(llion): Add caching.
+            y = common_attention.multihead_attention(
+                common_layers.layer_preprocess(x, hparams),
+                encoder_output,
+                encoder_decoder_attention_bias,
+                hparams.attention_key_channels or hparams.hidden_size,
+                hparams.attention_value_channels or hparams.hidden_size,
+                hparams.hidden_size, hparams.num_heads,
+                hparams.attention_dropout)
+            x = common_layers.layer_postprocess(x, y, hparams)
+        # Add target roots without pre- or postprocessing
+        x += extended_target_roots
+        with tf.variable_scope("ffn"):
+          y = transformer_ffn_layer(
+              common_layers.layer_preprocess(x, hparams), hparams)
+          x = common_layers.layer_postprocess(x, y, hparams)
+    # if normalization is done in layer_preprocess, then it shuold also be done
+    # on the output, since the output can grow very large, being the sum of
+    # a whole stack of unnormalized layer outputs.
+    return common_layers.layer_preprocess(x, hparams)
+
+
+def layerbylayer_transformer_add_target_roots(x, target_roots):
+  return x
+
+
+# Rest of the file is copied from tensor2tensor.models.transformer.
+# Importing these functions would confuse the registry.
+
+
+def transformer_ffn_layer(x, hparams, pad_remover=None):
+  """Copied from tensor2tensor.models.transformer."""
+  if hparams.ffn_layer == "conv_hidden_relu":
+    # In simple convolution mode, use `pad_remover` to speed up processing.
+    if pad_remover:
+      original_shape = tf.shape(x)
+      # Collapse `x` across examples, and remove padding positions.
+      x = tf.reshape(x, tf.concat([[-1], tf.shape(x)[2:]], axis=0))
+      x = tf.expand_dims(pad_remover.remove(x), axis=0)
+    conv_output = common_layers.conv_hidden_relu(
+        x,
+        hparams.filter_size,
+        hparams.hidden_size,
+        dropout=hparams.relu_dropout)
+    if pad_remover:
+      # Restore `conv_output` to the original shape of `x`, including padding.
+      conv_output = tf.reshape(
+          pad_remover.restore(tf.squeeze(conv_output, axis=0)), original_shape)
+    return conv_output
+  elif hparams.ffn_layer == "parameter_attention":
+    return common_attention.parameter_attention(
+        x, hparams.parameter_attention_key_channels or hparams.hidden_size,
+        hparams.parameter_attention_value_channels or hparams.hidden_size,
+        hparams.hidden_size, hparams.filter_size, hparams.num_heads,
+        hparams.attention_dropout)
+  elif hparams.ffn_layer == "conv_hidden_relu_with_sepconv":
+    return common_layers.conv_hidden_relu(
+        x,
+        hparams.filter_size,
+        hparams.hidden_size,
+        kernel_size=(3, 1),
+        second_kernel_size=(31, 1),
+        padding="LEFT",
+        dropout=hparams.relu_dropout)
+  else:
+    assert hparams.ffn_layer == "none"
+    return x
+
+
+def transformer_prepare_encoder(inputs, target_space, hparams):
+  """Copied from tensor2tensor.models.transformer."""
+  ishape_static = inputs.shape.as_list()
+  encoder_input = inputs
+  encoder_padding = common_attention.embedding_to_padding(encoder_input)
+  ignore_padding = common_attention.attention_bias_ignore_padding(
+      encoder_padding)
+  encoder_self_attention_bias = ignore_padding
+  encoder_decoder_attention_bias = ignore_padding
+  if hparams.proximity_bias:
+    encoder_self_attention_bias += common_attention.attention_bias_proximal(
+        tf.shape(inputs)[1])
+  # Append target_space_id embedding to inputs.
+  emb_target_space = common_layers.embedding(
+      target_space, 32, ishape_static[-1], name="target_space_embedding")
+  emb_target_space = tf.reshape(emb_target_space, [1, 1, -1])
+  encoder_input += emb_target_space
+  if hparams.pos == "timing":
+    encoder_input = common_attention.add_timing_signal_1d(encoder_input)
+  return (encoder_input, encoder_self_attention_bias,
+          encoder_decoder_attention_bias)
+
+
+def transformer_encoder(encoder_input,
+                        encoder_self_attention_bias,
+                        hparams,
+                        name="encoder"):
+  """Copied from tensor2tensor.models.transformer."""
+  x = encoder_input
+  with tf.variable_scope(name):
+    pad_remover = None
+    if hparams.use_pad_remover:
+      pad_remover = expert_utils.PadRemover(
+          common_attention.attention_bias_to_padding(
+              encoder_self_attention_bias))
+    for layer in xrange(hparams.num_encoder_layers or
+                        hparams.num_hidden_layers):
+      with tf.variable_scope("layer_%d" % layer):
+        with tf.variable_scope("self_attention"):
+          y = common_attention.multihead_attention(
+              common_layers.layer_preprocess(x, hparams),
+              None,
+              encoder_self_attention_bias,
+              hparams.attention_key_channels or hparams.hidden_size,
+              hparams.attention_value_channels or hparams.hidden_size,
+              hparams.hidden_size,
+              hparams.num_heads,
+              hparams.attention_dropout,
+              attention_type=hparams.self_attention_type,
+              max_relative_position=hparams.max_relative_position)
+          x = common_layers.layer_postprocess(x, y, hparams)
+        with tf.variable_scope("ffn"):
+          y = transformer_ffn_layer(
+              common_layers.layer_preprocess(x, hparams), hparams, pad_remover)
+          x = common_layers.layer_postprocess(x, y, hparams)
+    # if normalization is done in layer_preprocess, then it shuold also be done
+    # on the output, since the output can grow very large, being the sum of
+    # a whole stack of unnormalized layer outputs.
+    return common_layers.layer_preprocess(x, hparams)
+
+
+def transformer_prepare_decoder(targets, hparams):
+  """Copied from tensor2tensor.models.transformer."""
+  decoder_self_attention_bias = (
+      common_attention.attention_bias_lower_triangle(tf.shape(targets)[1]))
+  if hparams.proximity_bias:
+    decoder_self_attention_bias += common_attention.attention_bias_proximal(
+        tf.shape(targets)[1])
+  decoder_input = common_layers.shift_right_3d(targets)
+  if hparams.pos == "timing":
+    decoder_input = common_attention.add_timing_signal_1d(decoder_input)
+  return (decoder_input, decoder_self_attention_bias)
+
