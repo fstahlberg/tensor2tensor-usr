b601b53e65df8f37f7ac868723d6dcf7ff5c6284
fstahlberg <fs439@jabba>
2017-12-14 22:06:39 +0000

    - Add new transformer hparams

--- usr/configs/hparams.py
+++ usr/configs/hparams.py
@@ -5,15 +5,17 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
 from tensor2tensor.utils import registry
-from tensor2tensor.models.transformer import transformer_base
+from tensor2tensor.models.transformer import transformer_base_v2
 
 
 @registry.register_hparams
-def transformer_base_12gb_gpu():
-  """HParams for transformer base model for a single 12GB gpu."""
-  hparams = transformer_base()
-  hparams.learning_rate_warmup_steps = 8000
+def transformer_base_v2_fake_replicas():
+  hparams = transformer_base_v2()
+  hparams.learning_rate_warmup_steps *= 8
+  hparams.learning_rate /= math.sqrt(8.0)
+  hparams.max_length = 150
   hparams.batch_size = 8192
   return hparams
 
@@ -29,3 +31,5 @@ def transformer_base_12gb_gpu_large_batch():
   hparams.fake_gpu_multiplier = 4
   hparams.optimizer = "LargebatchAdam"
   return hparams
+
+
