608f22d01c0e48f32eac4173b3ab90f4f239cc9b
fstahlberg <fs439@jabba>
2018-01-15 22:22:46 +0000

    Add problem

--- usr/configs/problem.py
+++ usr/configs/problem.py
@@ -182,3 +182,19 @@ class TranslateEnzhTcbpeWmt32k(TranslateEndeWmt32k):
     return {"inputs": text_encoder.TokenTextEncoder(src_vocab_filename), 
             "targets": text_encoder.TokenTextEncoder(trg_vocab_filename)}
 
+
+@registry.register_problem
+class TranslateZhenTcbpeWmt32k(TranslateEndeWmt32k):
+  @property
+  def src_vocab_file(self):
+    return "vocab.zh.%s" % self.name
+  @property
+  def trg_vocab_file(self):
+    return "vocab.en.%s" % self.name
+
+  def feature_encoders(self, data_dir):
+    src_vocab_filename = os.path.join(data_dir, self.src_vocab_file)
+    trg_vocab_filename = os.path.join(data_dir, self.trg_vocab_file)
+    return {"inputs": text_encoder.TokenTextEncoder(src_vocab_filename), 
+            "targets": text_encoder.TokenTextEncoder(trg_vocab_filename)}
+
